f1487a8765e9751df78716a87bb9509b0a5b61c9
bcortez-gomes <160198522+bcortez-gomes@users.noreply.github.com>
2024-04-28 16:30:01 -0300

    adiconando os arquivos ao github

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#cep').mask('00000-000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cell').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules: {
+            nomeCompleto: {
+                required: true
+            }
+            enderecoCompleto: {
+                required: true
+            }
+            cep: {
+                required: true
+            }
+            cpf: {
+                required: true
+            }
+            cell: {
+                required: true
+            }
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            nomeCompleto: 'Por favor, insira o seu nome completo.'
+        },
+
+    })
+})
+
